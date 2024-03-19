3ef9006f77a9d72c6cadf9ec61048778153d9fbc
Bernath Mark Bence <naruto010190@gmail.com>
2024-03-19 13:57:21 +0100

    Added firebase to the application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"solyomsoft-task","appId":"1:1026912427102:web:da0fd7a7bca814d2afa8cf","storageBucket":"solyomsoft-task.appspot.com","apiKey":"AIzaSyDfV_H2XvIWj6jFX4ojqNLjBFFSbG96lLs","authDomain":"solyomsoft-task.firebaseapp.com","messagingSenderId":"1026912427102"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
